619fe9897a4037279daac00272be468874735031
Ting Sun <sunting.05@gmail.com>
2018-07-11 11:23:42 +0100

    added python script to add SUEWS-specific input variables to wrfinput files

---
+++ mod-input-python/add-SUEWS-input.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+import xarray as xr
+import numpy as np
+import glob
+
+
+# load base nc file for modification
+ds_base = xr.open_dataset('wrfinput_d01.base.nc')
+
+
+# funcitons to add new variables with expanded dimensions
+def gen_var_expand(name, rules, var_base=ds_base['T2'].copy(deep=True)):
+    var_new = var_base.rename(name.upper())
+    var_attrs = var_base.attrs
+    (name_dim, axis, rep) = rules[0]
+    val_init = rules[1]
+    var_new = np.repeat(
+        var_new.expand_dims(
+            name_dim, axis=axis), rep, axis=axis)
+
+    var_new.attrs = var_attrs
+    var_new.attrs['MemoryOrder'] = 'XYZ'
+    var_new.attrs[u'description'] = name
+    var_new.attrs['stagger'] = 'Z'
+    # set default values
+    var_new = val_init
+
+    return var_new
+
+
+# funcitons to add new variables with the same dimensionality
+def gen_var_keep(name, rules, var_base=ds_base['T2'].copy(deep=True)):
+    var_new = var_base.rename(name)
+    var_new.attrs[u'description'] = name
+    val_init = rules[1]
+    # set default values
+    var_new = val_init
+
+    return var_new
+
+
+# A generic wrapper to generate a variable for SUEWS
+def gen_var(name, rules, var_base=ds_base['T2'].copy()):
+    if type(rules[0]) is list:
+        var_new = gen_var_expand(name, rules, var_base)
+    else:
+        var_new = gen_var_keep(name, rules, var_base)
+
+    return var_new
+
+
+# rules to create input variable needed by SUEWS
+# TODO: add default values
+# rule pattern:
+# var_name: ('new_dim', new_pos_axis, number_in_axis)
+dict_rules_suews = {
+    'LAI_SUEWS': [('lai', 1, 3), 2],
+    'albDecTr_SUEWS': [0, .1],
+    'albEveTr_SUEWS': [0, .1],
+    'albGrass_SUEWS': [0, .1],
+    'DecidCap_SUEWS': [0, 10],
+    'porosity_SUEWS': [0, 10],
+    'GDD_SUEWS': [('gdd', 1, 5), 0],
+    'HDD_SUEWS': [('hdd', 1, 12), 0],
+    'state_SUEWS': [('nsurf', 1, 7), 150],
+    'soilmoist_SUEWS': [('nsurf', 1, 7), 150],
+    'surf_var_SUEWS': [('nsurf', 1, 7), 10],
+    'qn1_av_SUEWS': [0, 0],
+    'qn1_s_SUEWS': [0, 0],
+    'dqndt_SUEWS': [0, 0],
+    'dqnsdt_SUEWS': [0, 0],
+    'MeltWaterStore': [('nsurf', 1, 7), 10],
+    'SnowAlb': [0, 10],
+    'WUDay': [('wu', 1, 9), 10],
+    'z0m_in': [0, .1],
+    'zdm_in': [0, .1]}
+
+
+fl_wrfinput = glob.glob('wrfinput*')
+
+
+# NB: variables in wrfinput have to be named in CAPITALISED strings
+ds_new = xr.Dataset({name.upper(): gen_var(name, rules)
+                     for name, rules in dict_rules_suews.items()})
+
+
+# merge with ds_base for export
+ds_merged = ds_base.update(ds_new)
+
+# amke sure SUEWS scheme option is set
+ds_merged.attrs['SF_SURFACE_PHYSICS'] = 9
+
+# delete 'coordinates' attribute as xarray is unhappy with it
+print 'Removing attr coordinates from:'
+for var in ds_merged.data_vars.keys():
+    if 'coordinates' in ds_merged[var].attrs:
+        print var
+        del ds_merged[var].attrs['coordinates']
+
+# export merged dataset to a new file
+ds_merged.to_netcdf('wrfinput_d01.new.nc', mode='w', format='NETCDF4_CLASSIC')
--- mod-input-python/modify_global_att.py
+++ mod-input-python/modify_global_att.py
@@ -2,7 +2,7 @@
 import netCDF4 as nc4
 import os
 
-filename = os.path.join('/Users/zhenkunli/work/2018/WRFV3/run', 'wrfinput_d01' )
+filename = os.path.join('/Users/zhenkunli/work/2018/WRFV3/run', 'wrfinput_d01')
 print 'Processing file %s...' % filename
 nc = nc4.Dataset(filename, 'a')
 nc.SF_SURFACE_PHYSICS = 9
