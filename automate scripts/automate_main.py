f7dbc5664cd50291503fbe6b567bf805124881f9
hamidrezaomidvar <omidvar.hamidreza@gmail.com>
2019-03-13 13:05:06 +0000

    Changed it to a function for modularity purposes

--- automate scripts/automate_main.py
+++ automate scripts/automate_main.py
@@ -1,22 +1,11 @@
 import pandas as pd
 import numpy as np
+import sub_modules_automate as sa
 
 filePath = '/Users/hamidrezaomidvar/Dropbox/WRF-SUEWS/automate scripts/'
 in_file = 'Registry.EM_COMMON'
 query = 'package   ssibscheme     sf_surface_physics==8'
 to_add = 'package   suewsscheme    sf_surface_physics==9       -             state:LAI_SUEWS,albDecTr_SUEWS,albEveTr_SUEWS,albGrass_SUEWS,DecidCap_SUEWS,porosity_SUEWS,GDD_SUEWS,HDD_SUEWS,state_SUEWS,soilmoist_SUEWS,surf_var_SUEWS,landusef_SUEWS,alb_SUEWS,emis_SUEWS,qn1_av_SUEWS,qn1_s_SUEWS,dqndt_SUEWS,dqnsdt_SUEWS,MeltWaterStore,SnowAlb,WUDay,z0m_in,zdm_in'
 
-print('Reading file:'+ in_file)
-with open(filePath+in_file,'r') as ifile: # Reading the file
-    buf=ifile.readlines()
+sa.find_add(filePath,in_file,query,to_add)
 
-out_file=in_file+'_mod'
-
-print('Writing file:'+out_file)
-with open(filePath+out_file,'w') as ofile:
-    for line in buf:
-        if(query in line):
-            line=line+'\n'+to_add+'\n'
-        ofile.write(line)
-
-print('Modifiying file='+in_file+' is finished')
---
+++ automate scripts/sub_modules_automate.py
@@ -0,0 +1,16 @@
+def find_add(filePath,in_file,query,to_add):
+
+    print('Reading file:'+ in_file)
+    with open(filePath+in_file,'r') as ifile: # Reading the file
+        buf=ifile.readlines()
+
+    out_file=in_file+'_mod'
+
+    print('Writing file:'+out_file)
+    with open(filePath+out_file,'w') as ofile:
+        for line in buf:
+            if(query in line):
+                line=line+'\n'+to_add+'\n'
+            ofile.write(line)
+
+    print('Modifiying file='+in_file+' is finished')
