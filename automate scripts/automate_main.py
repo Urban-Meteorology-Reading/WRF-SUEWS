0bf2fcd9d19fc7c0d51af9fb8f35913cfe1a4d20
hamidrezaomidvar <omidvar.hamidreza@gmail.com>
2019-03-13 17:07:07 +0000

    Adding the changes that are needed for the coupling to the list file

--- automate scripts/automate_main.py
+++ automate scripts/automate_main.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import numpy as np
 import sub_modules_automate as sa
+import json
 
-filePath = '/Users/hamidrezaomidvar/Dropbox/WRF-SUEWS/automate scripts/'
-in_file = 'Registry.EM_COMMON'
-query = 'package   ssibscheme     sf_surface_physics==8'
-to_add = 'package   suewsscheme    sf_surface_physics==9       -             state:LAI_SUEWS,albDecTr_SUEWS,albEveTr_SUEWS,albGrass_SUEWS,DecidCap_SUEWS,porosity_SUEWS,GDD_SUEWS,HDD_SUEWS,state_SUEWS,soilmoist_SUEWS,surf_var_SUEWS,landusef_SUEWS,alb_SUEWS,emis_SUEWS,qn1_av_SUEWS,qn1_s_SUEWS,dqndt_SUEWS,dqnsdt_SUEWS,MeltWaterStore,SnowAlb,WUDay,z0m_in,zdm_in'
 
-sa.find_add(filePath,in_file,query,to_add)
+change_list=json.load(open('changes_list.dict'))
+
+for key in change_list.keys():
+    in_file = key
+    filePath = change_list[key]["filePath"]
+    query = change_list[key]["query"]
+    for qkey in query.keys():
+        sa.find_add(filePath, in_file, qkey, query[qkey])
 
--- automate scripts/sub_modules_automate.py
+++ automate scripts/sub_modules_automate.py
@@ -4,13 +4,13 @@ def find_add(filePath,in_file,query,to_add):
     with open(filePath+in_file,'r') as ifile: # Reading the file
         buf=ifile.readlines()
 
-    out_file=in_file+'_mod'
+    out_file=in_file
 
     print('Writing file:'+out_file)
     with open(filePath+out_file,'w') as ofile:
         for line in buf:
             if(query in line):
-                line=line+'\n'+to_add+'\n'
+                line=line+to_add+'\n'
             ofile.write(line)
 
-    print('Modifiying file='+in_file+' is finished')
+    print('Modifiying file='+in_file+' is finished\n')
