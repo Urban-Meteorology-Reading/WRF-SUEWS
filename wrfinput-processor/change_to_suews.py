2b70c4cb07078db27f4dfcbb2cab99c65fc3f0a0
Hamidreza Omidvar <omidvar.hamidreza@gmail.com>
2019-04-23 16:29:21 +0100

    adding capability of urban mask for urban related parameters

--- wrfinput-processor/change_to_suews.py
+++ wrfinput-processor/change_to_suews.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import yaml
+yaml.warnings({'YAMLLoadWarning': False})
 import xarray as xr
 import numpy as np
 import json
@@ -11,26 +13,27 @@ ds_base = xr.open_dataset('wrfinput_d01')
 
 
 # funcitons to add new variables with expanded dimensions
+
+
 def gen_var_expand(var_key, vars_to_add, var_base=ds_base['T2'].copy(deep=True)):
     var_new = var_base.rename(var_key.upper())
     var_attrs = var_base.attrs
-    (name_dim, axis, rep) = (vars_to_add[var_key]['d_name'],
+    (name_dim, axis, dim) = (vars_to_add[var_key]['dim_name'],
                             vars_to_add[var_key]['axis'],
-                            vars_to_add[var_key]['rep'])
+                            vars_to_add[var_key]['dim'])
     val_init = vars_to_add[var_key]['value']
     var_new = np.repeat(
         var_new.expand_dims(
-            name_dim, axis=axis), rep, axis=axis)
+            name_dim, axis=axis), dim, axis=axis)
 
     var_new.attrs = var_attrs
     var_new.attrs['MemoryOrder'] = 'XYZ'
     var_new.attrs[u'description'] = var_key
     var_new.attrs['stagger'] = 'Z'
-    var_new.attrs['units'] = vars_to_add[var_key]['units']
+    var_new.attrs['unit'] = vars_to_add[var_key]['unit']
     # set default values
-    # var_new.values = np.ones_like(var_new.values) * val_init
-    for i in range(0,rep):
-        var_new.values[:,i,:,:]=val_init[i]
+    for i in range(0, dim):
+        var_new.values[:, i, :, :] = val_init[i]
     return var_new
 
 
@@ -38,7 +41,7 @@ def gen_var_expand(var_key, vars_to_add, var_base=ds_base['T2'].copy(deep=True))
 def gen_var_keep(var_key, vars_to_add, var_base=ds_base['T2'].copy(deep=True)):
     var_new = var_base.rename(var_key.upper())
     var_new.attrs[u'description'] = var_key
-    var_new.attrs['units'] = vars_to_add[var_key]['units']
+    var_new.attrs['unit'] = vars_to_add[var_key]['unit']
     val_init = vars_to_add[var_key]['value']
     # set default values
     var_new.values = np.ones_like(var_new.values) * val_init
@@ -47,26 +50,48 @@ def gen_var_keep(var_key, vars_to_add, var_base=ds_base['T2'].copy(deep=True)):
 
 
 # A generic wrapper to generate a variable for SUEWS
-def gen_var(var_key, vars_to_add, var_base=ds_base['T2'].copy()):
-    if (vars_to_add[var_key]['rep']!=0):
+def gen_var(var_key, vars_to_add, landuse_mask, var_base=ds_base['T2'].copy()):
+    if (vars_to_add[var_key]['dim'] != 0):
         var_new = gen_var_expand(var_key, vars_to_add, var_base)
+        landuse_mask_n = np.repeat(
+            landuse_mask[None, ...], var_new.values.shape[1], axis=0)
+        landuse_mask_n = np.repeat(
+            landuse_mask_n[None, ...], var_new.values.shape[0], axis=0)
     else:
         var_new = gen_var_keep(var_key, vars_to_add, var_base)
+        landuse_mask_n = np.repeat(
+            landuse_mask[None, ...], var_new.values.shape[0], axis=0)
+
+    if (vars_to_add[var_key]['mask'] == 'urban'):
+        var_new.values = np.multiply(var_new.values, landuse_mask_n)
     return var_new
 
 
 # variables to be added to wrfinputs
-with open('../SUEWS_variables.json') as var_json:
+with open('../SUEWS_param.json') as var_json:
     vars_to_add = json.load(var_json)
 
+
+# finding urban masks
+
+def urban_mask(ds_base):
+    urban_category = 12
+    landusef = ds_base['LANDUSEF'].values[0, urban_category, :, :]
+    landuse_mask = landusef
+    landuse_mask[landusef > 0] = 1
+    return landuse_mask
+
+
 # added SUEWS required input to a single wrfinput file
+
 def add_SUEWS_wrfinput_single(x_file):
-    print('working on:'+ x_file)
+    print('working on:' + x_file)
     # get base file
     ds_base = xr.open_dataset(x_file)
+    landuse_mask = urban_mask(ds_base)
     # NB: variables in wrfinput have to be named in CAPITALISED strings
     ds_new = xr.Dataset({
-        var_key.upper(): gen_var(var_key,vars_to_add, ds_base['T2'].copy(deep=True))
+        var_key.upper(): gen_var(var_key, vars_to_add, landuse_mask, ds_base['T2'].copy(deep=True))
         for var_key in vars_to_add.keys()})
 
     # merge with ds_base for export
@@ -79,17 +104,16 @@ def add_SUEWS_wrfinput_single(x_file):
     ds_merged.attrs['SF_SURFACE_PHYSICS'] = 9
 
     # delete 'coordinates' attribute as xarray is unhappy with it
-    # print('Removing attr coordinates from:')
+
     for var in ds_merged.data_vars.keys():
         if 'coordinates' in ds_merged[var].attrs:
-            # print(var)
             del ds_merged[var].attrs['coordinates']
-    # ds_merged['LAI_SUEWS']
+
     # export merged dataset to a new file
     file_out = x_file+'.suews'
     ds_merged.to_netcdf(file_out,
                         mode='w', format='NETCDF3_64BIT')
-    print('SUEWS input has beened added to:'+ file_out)
+    print('SUEWS input has beened added to:' + file_out)
 
     return file_out
 
