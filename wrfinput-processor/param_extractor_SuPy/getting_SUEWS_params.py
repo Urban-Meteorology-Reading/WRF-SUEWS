837940b18e331abd3ed20ef66998c4b6f8d37015
Hamidreza Omidvar <omidvar.hamidreza@gmail.com>
2019-05-10 12:04:32 +0100

    adding spin up capability to getting_SUEWS_params.py

--- wrfinput-processor/param_extractor_SuPy/getting_SUEWS_params.py
+++ wrfinput-processor/param_extractor_SuPy/getting_SUEWS_params.py
@@ -1,15 +1,63 @@
 # %%
-#import yaml
-#yaml.warnings({'YAMLLoadWarning': False})
+import yaml
+yaml.warnings({'YAMLLoadWarning': False})
 import json
 from pathlib import Path
 import supy as sp
 import numpy as np
 import f90nml
+from datetime import datetime
+import pandas as pd
 
-path_runcontrol = Path('sample_run') / 'RunControl.nml'
-df_state_init = sp.init_supy(path_runcontrol)
+def spinup_SUEWS():
 
+    print('Initializing SUEWS variables.....')
+    df_state_init, df_forcing = sp.load_SampleData()
+
+    path_runcontrol = Path('./sample_run_mod') / 'RunControl.nml'
+    df_state_init = sp.init_supy(path_runcontrol)
+
+    first_day_str='2012-4-10'
+    first_day=datetime.strptime(first_day_str, '%Y-%m-%d')
+
+    print('Rotating the time based on the first day of '+first_day_str)
+    first_part=df_forcing.loc[df_forcing[df_forcing.index>=first_day].index]
+    second_part=df_forcing.loc[df_forcing[
+        df_forcing.index<first_day-pd.Timedelta('1 days')
+        ].index]
+    second_part.index=second_part.index+pd.Timedelta('366 days')
+
+    df_forcing_2=first_part.append(second_part)
+    df_forcing_2.iy=df_forcing_2.index.year
+    df_forcing_2.index.freq=first_part.index.freq
+
+    round_number=0
+    error=1000
+    while (error >= 0.5):
+        round_number=round_number+1
+        print('Running SUEWS for round number '+str(round_number)+'.....')
+        df_output, df_state_final = sp.run_supy(
+                                    df_forcing_2, df_state_init,
+                                    save_state=False)
+        final_state=df_state_final[ df_state_init.columns.levels[0]
+                                ].iloc[1]
+        df_state_init.iloc[0]=final_state
+        soilstore_before=df_state_final.soilstore_id.iloc[0]
+        soilstore_after =df_state_final.soilstore_id.iloc[1]
+        diff_soil=sum(abs(soilstore_after-soilstore_before))
+        error=100*diff_soil/soilstore_before.mean()
+        print('Change in soil store in % = '+str(error))
+
+    print('SUEWS spinup is finished. Total number of runs = '+ str(round_number))
+    
+    return df_state_init
+
+
+df_state_init = spinup_SUEWS()
+
+print('Putting NetRadiationMethod = 1')
+df_state_init.netradiationmethod=1
+df_state_init.rename(columns={'soilstore_id':'soilmoist'},inplace=True)
 ##################### JSON ######################################
 
 with open('../SUEWS_param.json') as suews_file:
