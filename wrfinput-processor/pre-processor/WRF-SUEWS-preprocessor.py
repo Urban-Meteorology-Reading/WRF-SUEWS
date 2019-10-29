89e587d119a7a93fc5e0308779cc32eec92a02f3
Hamidreza Omidvar <omidvar.hamidreza@gmail.com>
2019-10-29 15:15:20 +0000

    some cleaning and improvment of pre processing

--- wrfinput-processor/pre-processor/WRF-SUEWS-preprocessor.py
+++ wrfinput-processor/pre-processor/WRF-SUEWS-preprocessor.py
@@ -18,7 +18,7 @@ first_day_str = '2012-04-10'
 if steps['extract_params'] == 1:
     print('Extracting SUEWS parameters . . . ')
     for cityname in citynames:
-        print('preparing for London')
+        print('preparing for '+cityname+' . . .')
         getting_SUEWS_params(cityname, first_day_str)
     
 ################################################
@@ -30,22 +30,27 @@ if steps['change_to_SUEWS'] == 1:
 #### all specific modification should be done here!!!
     
 ################################################
+finalize=0
 if steps['g1_g6'] == 1:
     print('Modifying g1-g6 . . .')
     g1_g6()
     out='output/2-g1_g6_changed/'
+    finalize=1
 ################################################
 if steps['timezone'] == 1:
     print('Changing timezone values . . . ')
     set_timezone()
     out='output/3-timzone_changed'
+    finalize=1
 
 #%%
-src_files = os.listdir(out)
-for file_name in src_files:
-    full_file_name = os.path.join(out, file_name)
-    if os.path.isfile(full_file_name):
-        print('copying '+full_file_name+' to '+'output/final')
-        shutil.copy(full_file_name, 'output/final')
+
+if finalize==1:
+    src_files = os.listdir(out)
+    for file_name in src_files:
+        full_file_name = os.path.join(out, file_name)
+        if os.path.isfile(full_file_name):
+            print('copying '+full_file_name+' to '+'output/final')
+            shutil.copy(full_file_name, 'output/final')
 
 # %%
