d862ec70b9e76d2503fb3e401061afd192236aa5
Ting Sun <sunting.05@gmail.com>
2021-10-14 14:03:01 +0100

    update run-wrf job script

--- jasmin-config/sb-run_wrf.sh
+++ jasmin-config/sb-run_wrf.sh
@@ -2,10 +2,14 @@
 # sb-run_wrf.sh
 
 #SBATCH -p par-multi
-#SBATCH -n 36
+#SBATCH -n 48
 #SBATCH -o %j.log
 #SBATCH -e %j.err
 #SBATCH --time=48:00
+
+# load MPI lib
+# https://help.jasmin.ac.uk/article/4896-how-to-submit-an-mpi-parallel-job-to-slurm
+module load eb/OpenMPI/intel/3.1.1
 
 echo "Running WRF"
 mpirun ./wrf.exe
